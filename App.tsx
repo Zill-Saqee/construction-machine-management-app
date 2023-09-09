0d60178d8451068acc768cdc3cc25ecfe0841d34
Zill-Saqee <zillsaqee@gmail.com>
2023-09-09 20:15:01 +0500

    changes

--- App.tsx
+++ App.tsx
@@ -1,16 +1,30 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {Provider as PaperProvider} from 'react-native-paper';
-// import AppNavigator from './src/navigation/AppNavigator';
-import {Provider} from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { Provider } from 'react-redux';
 import store from './src/store';
 import DrawerNavigator from './src/navigation/DrawerNavigation';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'blue',
+    accent: 'green',
+  },
+  fonts: {
+    ...DefaultTheme.fonts,
+    regular: 'Roboto',
+    medium: 'Roboto-Medium',
+  },
+  background: 'lightblue',
+};
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <PaperProvider>
+      <PaperProvider theme={theme}>
         <NavigationContainer>
           <DrawerNavigator />
         </NavigationContainer>
